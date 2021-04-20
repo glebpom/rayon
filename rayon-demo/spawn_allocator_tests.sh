6434549038d3aa3fa1166ea4b059106a787916ae
Gleb Pomykalov <gleb@exogress.com>
2021-04-20 22:25:14 +0300

    time

--- rayon-demo/spawn_allocator_tests.sh
+++ rayon-demo/spawn_allocator_tests.sh
@@ -3,65 +3,65 @@
 echo "System allocator"
 echo "================"
 echo "life >>>"
-cargo run -q --release -- life bench
+/usr/bin/time -v cargo run -q --release -- life bench
 echo "nbody >>>"
-cargo run -q --release -- nbody bench
+/usr/bin/time -v cargo run -q --release -- nbody bench
 echo "sieve >>>"
-cargo run -q --release -- sieve bench
+/usr/bin/time -v cargo run -q --release -- sieve bench
 echo "matmul >>>"
-cargo run -q --release -- matmul bench
+/usr/bin/time -v cargo run -q --release -- matmul bench
 echo "mergesort >>>"
-cargo run -q --release -- mergesort bench
+/usr/bin/time -v cargo run -q --release -- mergesort bench
 
 echo "tcmalloc"
 echo "========"
 echo "life >>>"
-cargo run -q --release --features=tcmalloc -- life bench
+/usr/bin/time -v cargo run -q --release --features=tcmalloc -- life bench
 echo "nbody >>>"
-cargo run -q --release --features=tcmalloc -- nbody bench
+/usr/bin/time -v cargo run -q --release --features=tcmalloc -- nbody bench
 echo "sieve >>>"
-cargo run -q --release --features=tcmalloc -- sieve bench
+/usr/bin/time -v cargo run -q --release --features=tcmalloc -- sieve bench
 echo "matmul >>>"
-cargo run -q --release --features=tcmalloc -- matmul bench
+/usr/bin/time -v cargo run -q --release --features=tcmalloc -- matmul bench
 echo "mergesort >>>"
-cargo run -q --release --features=tcmalloc -- mergesort bench
+/usr/bin/time -v cargo run -q --release --features=tcmalloc -- mergesort bench
 
 echo "jemallocator"
 echo "============"
 echo "life >>>"
-cargo run -q --release --features=jemallocator -- life bench
+/usr/bin/time -v cargo run -q --release --features=jemallocator -- life bench
 echo "nbody >>>"
-cargo run -q --release --features=jemallocator -- nbody bench
+/usr/bin/time -v cargo run -q --release --features=jemallocator -- nbody bench
 echo "sieve >>>"
-cargo run -q --release --features=jemallocator -- sieve bench
+/usr/bin/time -v cargo run -q --release --features=jemallocator -- sieve bench
 echo "matmul >>>"
-cargo run -q --release --features=jemallocator -- matmul bench
+/usr/bin/time -v cargo run -q --release --features=jemallocator -- matmul bench
 echo "mergesort >>>"
-cargo run -q --release --features=jemallocator -- mergesort bench
+/usr/bin/time -v cargo run -q --release --features=jemallocator -- mergesort bench
 
 echo "mimalloc"
 echo "========"
 echo "life >>>"
-cargo run -q --release --features=mimalloc -- life bench
+/usr/bin/time -v cargo run -q --release --features=mimalloc -- life bench
 echo "nbody >>>"
-cargo run -q --release --features=mimalloc -- nbody bench
+/usr/bin/time -v cargo run -q --release --features=mimalloc -- nbody bench
 echo "sieve >>>"
-cargo run -q --release --features=mimalloc -- sieve bench
+/usr/bin/time -v cargo run -q --release --features=mimalloc -- sieve bench
 echo "matmul >>>"
-cargo run -q --release --features=mimalloc -- matmul bench
+/usr/bin/time -v cargo run -q --release --features=mimalloc -- matmul bench
 echo "mergesort >>>"
-cargo run -q --release --features=mimalloc -- mergesort bench
+/usr/bin/time -v cargo run -q --release --features=mimalloc -- mergesort bench
 
 echo "rpmalloc"
 echo "========"
 echo "life >>>"
-cargo run -q --release --features=rpmalloc -- life bench
+/usr/bin/time -v cargo run -q --release --features=rpmalloc -- life bench
 echo "nbody >>>"
-cargo run -q --release --features=rpmalloc -- nbody bench
+/usr/bin/time -v cargo run -q --release --features=rpmalloc -- nbody bench
 echo "sieve >>>"
-cargo run -q --release --features=rpmalloc -- sieve bench
+/usr/bin/time -v cargo run -q --release --features=rpmalloc -- sieve bench
 echo "matmul >>>"
-cargo run -q --release --features=rpmalloc -- matmul bench
+/usr/bin/time -v cargo run -q --release --features=rpmalloc -- matmul bench
 echo "mergesort >>>"
-cargo run -q --release --features=rpmalloc -- mergesort bench
+/usr/bin/time -v cargo run -q --release --features=rpmalloc -- mergesort bench
 
